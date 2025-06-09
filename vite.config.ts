fe89f325323679c7d6cf05bbd705dac47f81ed93
jogit64 <usersfdc18@gmail.com>
2025-06-09 14:49:32 +0200

    Fix 4 injection des variables env pour Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
-import dotenv from "dotenv";
-
-dotenv.config(); // ← ajoute cette ligne
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
