e187c1933f601d6412a666924644f121ceabc23e
jogit64 <usersfdc18@gmail.com>
2025-06-09 14:29:10 +0200

    Log env vars pour debug Vercel

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,12 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+
+console.log("🔥 VITE_SUPABASE_URL", import.meta.env.VITE_SUPABASE_URL);
+console.log(
+  "🔥 VITE_SUPABASE_ANON_KEY",
+  import.meta.env.VITE_SUPABASE_ANON_KEY
+);
+console.log("🔥 VITE_TMDB_API_KEY", import.meta.env.VITE_TMDB_API_KEY);
 
 createRoot(document.getElementById("root")!).render(<App />);
